d03ee72807bfd0b6dd23fb33a5894a2b58a4c44b
luedu1103 <luis.calle3@unmsm.edu.pe>
2023-11-01 15:19:12 -0500

    Orden

---
+++ front/scripts/index.js
@@ -0,0 +1,20 @@
+//scroll reveal
+const sr = ScrollReveal({
+    distance:'65px',
+    duration:2600,
+    delay:450,
+    reset:false,
+});
+
+sr.reveal('.inicio-text',{delay:800, origin:'right'});
+sr.reveal('.inicio-img',{delay:450, origin:'left'});
+sr.reveal('.form-box',{delay:450, origin:'top'});
+
+// a mainPUNTOhtml CHI
+document.getElementById("loginbutton").addEventListener("click", function () {
+    window.location.href = "main.html";
+});
+
+
+
+ 
