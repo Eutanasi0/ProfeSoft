0bb4b3578c4eb653bbab722298540cddb25006a0
Benja <benjaminflores4e@gmail.com>
2023-11-20 12:15:37 -0500

    ola

--- back/front/scripts/dicTree.js
+++ back/front/scripts/dicTree.js
@@ -1,48 +1,7 @@
-<<<<<<< HEAD
-class TrieNode {
-    constructor() {
-        this.children = {};
-        this.isEndOfWord = false;
-        this.profesorData = null;
-    }
-}
-
-class Trie {
-    constructor() {
-        this.root = new TrieNode();
-    }
-
-    insert(word, data) {
-        let node = this.root;
-        for (let char of word) {
-            if (!node.children[char]) {
-                node.children[char] = new TrieNode();
-            }
-            node = node.children[char];
-        }
-        node.isEndOfWord = true;
-        node.profesorData = data;
-    }
-
-    search(query) {
-        let node = this.root;
-        for (let char of query) {
-            if (node.children[char]) {
-                node = node.children[char];
-            } else {
-                return null; // No match found
-            }
-        }
-        return node.isEndOfWord ? node.profesorData : null;
-    }
-}
-=======
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('user');
->>>>>>> 3f234888ea96d8134f6e350d9107ea3327730f1a
 
 let profesoresDB;
-let trie;
 
 async function fetchData() {
     try {
@@ -60,15 +19,6 @@ async function fetchData() {
 
         const data = await response.json();
         profesoresDB = data;
-
-        // Build trie from profesoresDB
-        trie = new Trie();
-        for (let profesor of profesoresDB.dataSend) {
-            let tempo_name = profesor.teacher_name.toLowerCase();
-            let tempo_course = profesor.course_name.toLowerCase();
-            trie.insert(tempo_name, profesor);
-            trie.insert(tempo_course, profesor);
-        }
     } catch (error) {
         console.error(error);
     }
@@ -84,8 +34,6 @@ async function buscarProfe() {
         if (!profesoresDB) {
             await fetchData(); // Fetch data if not already fetched
         }
-<<<<<<< HEAD
-=======
         for (let profesor of profesoresDB.dataSend) {
             let tempo_name = profesor.teacher_name.toLowerCase();
             let tempo_course = profesor.course_name.toLowerCase();
@@ -94,23 +42,14 @@ async function buscarProfe() {
                 const enlace = document.createElement('a');
                 enlace.href = `profes.html?user=${username}&id=${profesor.teacher_id}`; // Coloca la URL a la que deseas que apunte el enlace
                 enlace.textContent = `${profesor.teacher_name} - Curso: ${profesor.course_name}`;
->>>>>>> 3f234888ea96d8134f6e350d9107ea3327730f1a
 
-        // Search using trie
-        let result = trie.search(consulta);
+                // Crear un elemento de lista y agregar el enlace a él
+                const resultadoItem = document.createElement('li');
+                resultadoItem.appendChild(enlace);
 
-        if (result) {
-            // Crear un enlace y agregarlo al resultadoItem
-            const enlace = document.createElement('a');
-            enlace.href = 'profes.html'; // Coloca la URL a la que deseas que apunte el enlace
-            enlace.textContent = `${result.teacher_name} - Curso: ${result.course_name}`;
-
-            // Crear un elemento de lista y agregar el enlace a él
-            const resultadoItem = document.createElement('li');
-            resultadoItem.appendChild(enlace);
-
-            // Agregar el elemento de lista a la lista de resultados
-            resultados.appendChild(resultadoItem);
+                // Agregar el elemento de lista a la lista de resultados
+                resultados.appendChild(resultadoItem);
+            }
         }
     } catch (error) {
         console.error("Error during search:", error);
