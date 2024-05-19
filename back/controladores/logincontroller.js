40e984212ec5bb9e71a94499a58903d1a267893f
Eduardo_Calle <luis.calle3@unmsm.edu.pe>
2024-05-19 01:06:30 -0500

    Update logincontroller.js

--- back/controladores/logincontroller.js
+++ back/controladores/logincontroller.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres',
+    user: 'professoft',
     host: process.env.HOST,
     database: 'professoft',
     password: process.env.PASSWORD,
